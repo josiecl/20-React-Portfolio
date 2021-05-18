39719203e339df6a061b59efa1014604a90363c6
Josie <josiecollinsrc@gmail.com>
2021-05-18 16:43:46 -0500

    made navbar and footer sticky

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,10 +3,12 @@ import "./style.css";
 
 function Footer() {
   return (
-    <footer className="footer bg-dark">
-        <a className="link" href="https://github.com/josiecl">Github</a>
-        <a className="link" href="https://www.linkedin.com/in/josephine-collins-aa23201b9/">Linkedin</a>
-    </footer>
+    <div className="sticky-bottom">
+      <footer className="footer bg-dark">
+          <a className="link" href="https://github.com/josiecl">Github</a>
+          <a className="link" href="https://www.linkedin.com/in/josephine-collins-aa23201b9/">Linkedin</a>
+      </footer>
+    </div>
   );
 }
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,48 +2,49 @@ import React from "react";
 
 function Header() {
   return (
-      // Name is too close to end of navbar
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" style={{marginLeft: '10px'}} href="/">
-          Josie Collins
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
+      <div className="sticky-top">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a className="navbar-brand" style={{marginLeft: '10px'}} href="/">
+            Josie Collins
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
 
-        <section className="collapse navbar-collapse"  id="navbarSupportedContent">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="/Home">
-                Home
-              </a>
-            </li>
-            <li className="nav-item active">
-              <a className="nav-link" href="/Portfolio">
-                Portfolio
-              </a>
-            </li>
-            <li className="nav-item active">
-              <a className="nav-link" href="/Contact">
-                Contact
-              </a>
-            </li>
-            <li className="nav-item active">
-              <a className="nav-link" href="jcollresume.pdf" download>Download Resume</a>
-            </li>
+          <section className="collapse navbar-collapse"  id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="/Home">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item active">
+                <a className="nav-link" href="/Portfolio">
+                  Portfolio
+                </a>
+              </li>
+              <li className="nav-item active">
+                <a className="nav-link" href="/Contact">
+                  Contact
+                </a>
+              </li>
+              <li className="nav-item active">
+                <a className="nav-link" href="jcollresume.pdf" download>Download Resume</a>
+              </li>
+              
+            </ul>
             
-          </ul>
-          
-        </section>
-      </nav>
+          </section>
+        </nav>
+      </div>
       
 
   );
