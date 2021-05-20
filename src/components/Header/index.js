35e0d4dddcb68948b970cdbeec6a132cbea11431
Josie <josiecollinsrc@gmail.com>
2021-05-20 16:02:42 -0500

    got mobile navbar to work

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,56 +1,42 @@
 import React from "react";
-import { Nav } from "react-bootstrap";
+// import { Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import FormControl from "react-bootstrap/FormControl";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 function Header() {
   return (
       <div className="sticky-top">
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <Navbar className="navbar navbar-expand-lg navbar-dark bg-dark" expand="lg">
           <a className="navbar-brand" style={{marginLeft: '10px'}} href="/">
             Josie Collins
           </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
-
-          <section className="collapse navbar-collapse"  id="navbarSupportedContent">
+          
+        <Navbar.Toggle />
+          
+          <Navbar.Collapse>
             <ul className="navbar-nav ms-auto">
               <li className="nav-item active">
-                {/* <a className="nav-link" href="/Home">
-                  Home
-                </a> */}
                 <Nav.Link as={Link} to="/home">Home</Nav.Link>
               </li>
               <li className="nav-item active">
-                {/* <a className="nav-link" href="/Portfolio">
-                  Portfolio
-                </a> */}
                 <Nav.Link as={Link} to="/portfolio">Portfolio</Nav.Link>
               </li>
               <li className="nav-item active">
-                {/* <a className="nav-link" href="/Contact">
-                  Contact
-                </a> */}
                 <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
               </li>
               <li className="nav-item active">
                 <a className="nav-link" href="jcollresume.pdf" download>Download Resume</a>
               </li>
-              
             </ul>
-            
-          </section>
-        </nav>
+            </Navbar.Collapse>
+        </Navbar>
       </div>
-      
 
   );
 }
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 
+const base = "../../assets/"
+
+
 // Keeps giving error, unsure why
 function Project(props) {
     return (
         <div className="container">
             <section class="webInfo">
                 <h4 class="titleWord">{props.title}</h4>
-                <img src={props.image} class="img-fluid webImg" alt={props.title}></img>
+                <img src={require(`../../assets/${props.image}`)} class="img-fluid webImg" alt={props.title}></img>
                 <p>{props.description}</p>
-                <a href={props.repo}>Github Repo</a>
+                <a href={props.repo}>Github Repo</a> <br />
                 <a href={props.live}>Live website</a>
             </section>
         </div>
