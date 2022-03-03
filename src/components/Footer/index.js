e8f0aac99cebc27f89240bd4abb544c88676e57f
Collins <collinsj3252@my.uwstout.edu>
2022-03-03 14:51:13 -0600

    typo fix

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div className="sticky-bottom">
       <footer className="footer bg-dark">
-          <a className="link" href="https://github.com/josiecl">Github</a>
+          <a className="link" href="https://github.com/josiecl">GitHub</a>
           <a className="link" href="https://www.linkedin.com/in/josephine-collins-aa23201b9/">Linkedin</a>
       </footer>
     </div>
